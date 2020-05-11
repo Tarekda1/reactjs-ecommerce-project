1f7402b4c672325c5d581432626b0cf8c77bd085
Tarekda1 <tare2.da@gmail.com>
2020-05-11 09:17:39 -0400

    adding new changes

---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Directory } from "../../components/Directory";
+import "./syles.scss";
+
+const Homepage = (props) => {
+  return (
+    <section className="homePage">
+      <Directory />
+    </section>
+  );
+};
+
+export default Homepage;
